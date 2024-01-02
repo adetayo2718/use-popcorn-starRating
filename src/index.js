e574915d2b320293852197763351da1263210388
adetayo2718 <adetayo2718@gmail.com>
2024-01-02 05:35:40 +0100

    App updated, StarRating has been created with the public API outlined.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [extRating, setExtRating] = useState(0);
+  return (
+    <div>
+      <StarRating color={"blue"} maxRating={10} onSetExtRate={setExtRating} />
+      <p>This movie was rated {extRating} on the box office!</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      size={28}
+      color="#fcc419"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      rate={3}
+    />
+    <div>
+      {Array.from({ length: 100 }, (_, i) => (
+        <span key={i}>-</span>
+      ))}
+    </div>
+    <StarRating />
+    <Test />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
